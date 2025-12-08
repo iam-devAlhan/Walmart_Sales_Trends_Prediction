4a05e6644fb1f2c94d498fc14d3918bd126cbada
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-12-08 21:07:16 +0500

    feat: Added UI Selectbox and Button for predicting sales

--- ui/streamlit.py
+++ ui/streamlit.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
 
-st.text("Streamlit UI")
+st.title("Walmart Sales Prediction for better stock managing")
+st.text("This demonstrates the walmart sales prediction based on the Walmart Sales Dataset, It predicts the sales according to given date and event such as Holiday and Non Holiday")
+
+
+date_input = st.date_input(label="Enter Date to continue")
+is_holiday = st.selectbox(label="Select Holiday", options=["Holiday", "Non Holiday"])
+predict = st.button("Predict")
+
+if predict:
+    st.metric("Predicted Sales", "70%")
