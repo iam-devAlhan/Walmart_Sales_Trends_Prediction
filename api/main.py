835d6f91c6b11b7f9c408a913af1fdb3a1ea084c
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-12-08 23:39:59 +0500

    fix: Reflect latest change

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,4 +14,9 @@ app.add_middleware(CORSMiddleware,
 @app.get("/predict")
 async def predict():
     pass
+
+
+@app.get("/")
+async def root():
+    return JSONResponse({"message": "ASGI Server running on Port 8000"}, status_code=200)
 
