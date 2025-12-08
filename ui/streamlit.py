2bcc884834d875dee246ae1cd9122041b517d9c4
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-12-08 21:09:31 +0500

    feat: Added mock metrics, later to replacer by calculating value

--- ui/streamlit.py
+++ ui/streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import requests
 
 st.title("Walmart Sales Prediction for better stock managing")
 st.text("This demonstrates the walmart sales prediction based on the Walmart Sales Dataset, It predicts the sales according to given date and event such as Holiday and Non Holiday")
@@ -9,5 +9,6 @@ date_input = st.date_input(label="Enter Date to continue")
 is_holiday = st.selectbox(label="Select Holiday", options=["Holiday", "Non Holiday"])
 predict = st.button("Predict")
 
+# Added mock data , later adding it prediction
 if predict:
     st.metric("Predicted Sales", "70%")
